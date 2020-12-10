5a012d7d0e8466343cc5d4e041097ecba16ce66d
koosdrost <koosdrost90@gmail.com>
2020-12-10 11:42:31 +0100

    fade in for panels and text update

--- src/app/projects/bio/bio.component.ts
+++ src/app/projects/bio/bio.component.ts
@@ -1,11 +1,5 @@
 import {Component, ViewEncapsulation} from '@angular/core';
 
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  type: string;
-}
 
 @Component({
   selector: 'bio',
@@ -13,12 +7,4 @@ export interface Tile {
   encapsulation: ViewEncapsulation.None,
 })
 export class BioComponent {
-
-  tiles: Tile[] = [
-    {type: 'type1', cols: 3, rows: 1, color: 'lightblue'},
-    {type: 'type2', cols: 1, rows: 2, color: 'lightgreen'},
-    {type: 'type3', cols: 1, rows: 1, color: 'lightpink'},
-    {type: 'type4', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
-
 }
